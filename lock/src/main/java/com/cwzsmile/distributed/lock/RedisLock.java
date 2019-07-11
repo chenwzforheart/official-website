88c826147ffd0050817d573da89c17417f4aad83
wenzheng.chen <wenzheng.chen@baozun.com>
2019-07-11 19:54:14 +0800

    增加Redis和Zk分布式锁

---
+++ lock/src/main/java/com/cwzsmile/distributed/lock/RedisLock.java
@@ -0,0 +1,54 @@
+package com.cwzsmile.distributed.lock;
+
+import org.redisson.Redisson;
+import org.redisson.RedissonRedLock;
+import org.redisson.RedissonWriteLock;
+import org.redisson.api.RLock;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author csh9016
+ * @date 2019/7/11
+ */
+public class RedisLock {
+
+    private RedisTemplate<String, String> redisTemplate;
+
+    private Redisson redisson;
+
+    /**
+     * setNX做分布式锁，获取锁
+     */
+    public void getNXLock() {
+        redisTemplate.opsForValue().setIfAbsent("lock_key", "lock_value", 30, TimeUnit.SECONDS);
+    }
+
+    /**
+     * 释放锁
+     */
+    public void releaseNXLock() {
+        //redisTemplate.execute();
+    }
+
+    /**
+     * redisson获取锁
+     */
+    public void redissonLock() {
+        RLock lock1 = redisson.getFairLock("lock1");
+        RLock lock2 = redisson.getFairLock("lock2");
+        RLock lock3 = redisson.getFairLock("lock3");
+        RedissonRedLock multiLock = new RedissonRedLock(lock1, lock2, lock3);
+        multiLock.lock();
+        multiLock.unlock();
+        //redisson.getRedLock(new RedissonWriteLock(null, "LL"));
+    }
+
+    /**
+     * redisson释放锁
+     */
+    public void releaseRedissonLock() {
+        //redisson.getRedLock(new RedissonWriteLock(null, "LL"));
+    }
+}
---
+++ lock/src/main/java/com/cwzsmile/distributed/lock/ZkLock.java
@@ -0,0 +1,15 @@
+package com.cwzsmile.distributed.lock;
+
+import org.apache.curator.framework.CuratorFramework;
+
+/**
+ * @author csh9016
+ * @date 2019/7/11
+ */
+public class ZkLock {
+
+    private CuratorFramework curatorFramework;
+
+    public void getLock() {
+    }
+}
