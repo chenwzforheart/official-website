314d76767a118137eaa473cfcdc42da576578f13
wenzheng.chen <wenzheng.chen@baozun.com>
2019-10-08 15:55:58 +0800

    分布式事物实现

---
+++ transaction/src/main/java/com/cwzsmile/distributed/transaction/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @author csh9016
+ * @date 2019/10/8
+ * 分布式事物控制
+ * 1.LCN事物，https://segmentfault.com/a/1190000016718525
+ * 2.可靠消息模式，通过MQ实现最终一致
+ */
+package com.cwzsmile.distributed.transaction;
