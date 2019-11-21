7fa245ffaa4ed420dda5ecfb0661c820ef4fa735
wenzheng.chen <wenzheng.chen@baozun.com>
2019-11-21 11:30:12 +0800

    微服务网关技术选型

---
+++ gateway/src/main/java/com/cwzsmile/distributed/gateway/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * @author csh9016
+ * @date 2019/11/21
+ *
+ * 分布式技术：(1)微服务，(2)Service Mesh
+ * (1.1)Dubbo
+ * (1.2)SpringCloud
+ * (2.1)SOFA Mesh
+ * 网关技术(网关应用 + 配置元数据)：认证鉴权，协议转换，流量控制，灰度发布
+ * (1.1)Soul
+ * (1.2)Zuul(弃用),Gateway
+ * (2.1)
+ * 推荐实践方案：
+ * Dubbo + soul + Zookeeper + Nacos 实现微服务框架
+ */
+package com.cwzsmile.distributed.gateway;
